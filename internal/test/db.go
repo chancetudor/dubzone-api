2d7ec95bfdc28be1f370a39af063fe36e19214f4
Chance Tudor <31376942+chancetudor@users.noreply.github.com>
2021-07-16 16:45:23 -0400

    got the entities structured as Go structs

--- internal/test/db.go
+++ internal/test/db.go
@@ -3,9 +3,9 @@ package test
 import (
 	dbx "github.com/go-ozzo/ozzo-dbx"
 	_ "github.com/lib/pq" // initialize posgresql for test
-	"github.com/qiangxue/go-rest-api/internal/config"
-	"github.com/qiangxue/go-rest-api/pkg/dbcontext"
-	"github.com/qiangxue/go-rest-api/pkg/log"
+	"github.com/chancetudor/dubzone-api/internal/config"
+	"github.com/chancetudor/dubzone-api/pkg/dbcontext"
+	"github.com/chancetudor/dubzone-api/pkg/log"
 	"path"
 	"runtime"
 	"testing"
