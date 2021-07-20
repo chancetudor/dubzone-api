630b59a36aa06fc13c20a70a32e25b33b038b34d
Chance Tudor <31376942+chancetudor@users.noreply.github.com>
2021-07-20 11:09:10 -0400

    comments
    added comments to keep track of endpoints

--- internal/api/dmgProfile.go
+++ internal/api/dmgProfile.go
@@ -1,3 +1,13 @@
 package api
 
 // DamageProfile endpoints live here
+/*
+r.HandleFunc("/dmgprofile", CreateDamageProfileEndpoint).Methods("POST")
+	r.HandleFunc("/dmgprofile", ReadDamageProfileEndpoint).Methods("GET")
+	r.HandleFunc("/dmgprofile", UpdateDamageProfileEndpoint).Methods("PUT")
+	r.HandleFunc("/dmgprofile", DeleteDamageProfileEndpoint).Methods("DELETE")
+ */
+
+// TODO we want to use filtering
+// e.g. GET /dmgprofile?weaponID=12345 -- this returns the dmgprofile for specific weapon ID
+// OR GET /loadouts?weaponname=XM4 -- this returns the dmgprofile for specific weapon name
--- internal/api/loadout.go
+++ internal/api/loadout.go
@@ -1,3 +1,10 @@
 package api
 
 // Loadout endpoints live here
+
+/*
+	r.HandleFunc("/loadout", CreateLoadoutEndpoint).Methods("POST")
+	r.HandleFunc("/loadout", ReadLoadoutEndpoint).Methods("GET")
+	r.HandleFunc("/loadout", UpdateLoadoutEndpoint).Methods("PUT")
+	r.HandleFunc("/loadout", DeleteLoadoutEndpoint).Methods("DELETE")
+ */
